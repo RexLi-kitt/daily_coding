7ec9b7a969847877c0f59ac28b2ad68df27a36e2
RexLi-kitt <lpeiyu465@gmail.com>
2026-09-20 19:09:55 +0800

    zuoye1

--- 日常学习/中缀表达式转后缀表达式.cpp
+++ 日常学习/中缀表达式转后缀表达式.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 #include<stack>
+#include<vector>
+#include<string>
+#include<cctype>
 using namespace std;
 
 int priority(char ch){
@@ -12,23 +15,49 @@ int priority(char ch){
 int main(){
     string s;
     stack<char> st;
-    string ans;
+    vector<string> ans;
+    bool needOperand = true;
 
-    cout << "请输入中缀表达式：";
-    cin >> s;
+    cout << "请输入整数中缀表达式：";
+    getline(cin, s);
 
     for(int i = 0;i < (int)s.size();i ++){
         char ch = s[i];
 
-        if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')){
-            ans += ch;
+        if(ch == ' ') continue;
+
+        if(isdigit(ch)){
+            if(!needOperand){
+                cout << "错误：两个操作数之间缺少运算符" << endl;
+                return 0;
+            }
+
+            string number;
+            while(i < (int)s.size() && isdigit(s[i])){
+                number += s[i];
+                i ++;
+            }
+            i --;
+
+            ans.push_back(number);
+            needOperand = false;
         }
         else if(ch == '('){
+            if(!needOperand){
+                cout << "错误：左括号前缺少运算符" << endl;
+                return 0;
+            }
             st.push(ch);
+            needOperand = true;
         }
         else if(ch == ')'){
+            if(needOperand){
+                cout << "错误：右括号前缺少操作数" << endl;
+                return 0;
+            }
+
             while(!st.empty() && st.top() != '('){
-                ans += st.top();
+                ans.push_back(string(1, st.top()));
                 st.pop();
             }
             if(st.empty()){
@@ -36,15 +65,22 @@ int main(){
                 return 0;
             }
             st.pop();
+            needOperand = false;
         }
         else if(ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^'){
+            if(needOperand){
+                cout << "错误：运算符位置错误" << endl;
+                return 0;
+            }
+
             while(!st.empty() && st.top() != '(' &&
                   (priority(st.top()) > priority(ch) ||
                   (priority(st.top()) == priority(ch) && ch != '^'))){
-                ans += st.top();
+                ans.push_back(string(1, st.top()));
                 st.pop();
             }
             st.push(ch);
+            needOperand = true;
         }
         else{
             cout << "错误：有非法字符" << endl;
@@ -52,14 +88,68 @@ int main(){
         }
     }
 
+    if(ans.empty()){
+        cout << "错误：表达式不能为空" << endl;
+        return 0;
+    }
+    if(needOperand){
+        cout << "错误：表达式末尾缺少操作数" << endl;
+        return 0;
+    }
+
     while(!st.empty()){
         if(st.top() == '('){
             cout << "错误：括号不匹配" << endl;
             return 0;
         }
-        ans += st.top();
+        ans.push_back(string(1, st.top()));
         st.pop();
     }
 
-    cout << "后缀表达式：" << ans << endl;
+    stack<long long> numbers;
+    for(int i = 0;i < (int)ans.size();i ++){
+        if(isdigit(ans[i][0])){
+            numbers.push(stoll(ans[i]));
+        }
+        else{
+            if(numbers.size() < 2){
+                cout << "错误：操作数数量不正确" << endl;
+                return 0;
+            }
+            long long b = numbers.top();
+            numbers.pop();
+            long long a = numbers.top();
+            numbers.pop();
+            long long value = 0;
+            if(ans[i] == "+") value = a + b;
+            else if(ans[i] == "-") value = a - b;
+            else if(ans[i] == "*") value = a * b;
+            else if(ans[i] == "/"){
+                if(b == 0){
+                    cout << "错误：除数不能为0" << endl;
+                    return 0;
+                }
+                value = a / b;
+            }
+            else if(ans[i] == "^"){
+                value = 1;
+                if(b < 0){
+                    cout << "错误：指数不能为负数" << endl;
+                    return 0;
+                }
+                for(int j = 0;j < b;j ++) value *= a;
+            }
+            numbers.push(value);
+        }
+    }
+
+    if(numbers.size() != 1){
+        cout << "错误：表达式缺少运算符" << endl;
+        return 0;
+    }
+
+    cout << "后缀表达式：";
+    for(int i = 0;i < (int)ans.size();i ++) cout << ans[i] << ' ';
+    cout << endl;
+    cout << "表达式的值：" << numbers.top() << endl;
 }
